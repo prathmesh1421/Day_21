5d19c92e01f16e7586922584cf5ca99d9f750203
prathmesh joshi <147234424+prathmesh1421@users.noreply.github.com>
2026-05-09 17:44:50 +0530

    Add CssFixDemo component with CSS fixes examples

---
+++ hospital-ui-project/CssFixDemo.jsx
@@ -0,0 +1,13 @@
+export default function CssFixDemo() {
+  return (
+    <div>
+      <h2>CSS Fixes</h2>
+
+      <div className="fix fix1">Fix 1: Relative Position</div>
+      <div className="fix fix2">Fix 2: Overflow Hidden</div>
+      <div className="fix fix3">Fix 3: Z-Index Fix</div>
+      <div className="fix fix4">Fix 4: Flex Alignment</div>
+      <div className="fix fix5">Fix 5: Wrap Issue</div>
+    </div>
+  );
+}
